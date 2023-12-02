656f794518be90e10fb8c0fe7b90e62b53946031
Folke Westergren <folkew@axis.com>
2023-12-02 20:19:29 +0100

    prettier :D

--- day-2/cubeGame.js
+++ day-2/cubeGame.js
@@ -9,15 +9,15 @@ fs.readFile("input.txt", function read(err, data) {
   let totalPartOne = 0;
   let totalPartTwo = 0;
 
-  let games = values.map(val => val.replace(/^[^:]*:/, '').trim())
-  games = games.map(val => val.replace(/;|,|lue|ed|reen/g, ''))
+  let games = values.map((val) => val.replace(/^[^:]*:/, "").trim());
+  games = games.map((val) => val.replace(/;|,|lue|ed|reen/g, ""));
 
   const checkIfPossible = (game) => {
     let cubes = {
-      'r': 12,
-      'g': 13,
-      'b': 14,
-    }
+      r: 12,
+      g: 13,
+      b: 14,
+    };
     let possible = true;
     while (game.length > 0) {
       let matches = game.match(/([^\s]+)\s([^\s]*)/);
@@ -27,25 +27,25 @@ fs.readFile("input.txt", function read(err, data) {
         possible = false;
         break;
       } else {
-        game = game.replace(matches[0], '');
+        game = game.replace(matches[0], "");
       }
     }
     return possible;
-  }
+  };
 
   const checkLowest = (game) => {
     let cubes = {
-      'r': 0,
-      'g': 0,
-      'b': 0,
-    }
+      r: 0,
+      g: 0,
+      b: 0,
+    };
     while (game.length > 0) {
       const matches = game.match(/([^\s]+)\s([^\s]*)/);
       if (matches === null) break;
 
       if (cubes[matches[2]] < matches[1] || cubes[matches[2]] === 0) {
-        cubes[matches[2]] = parseInt(matches[1])
-        game = game.replace(matches[0], "").trim()
+        cubes[matches[2]] = parseInt(matches[1]);
+        game = game.replace(matches[0], "").trim();
       } else {
         game = game.replace(matches[0], "").trim();
       }
@@ -54,13 +54,13 @@ fs.readFile("input.txt", function read(err, data) {
   };
 
   games.forEach((game, index) => {
-    if (checkIfPossible(game)) totalPartOne += index+1 
-  })
+    if (checkIfPossible(game)) totalPartOne += index + 1;
+  });
 
   games.forEach((game) => {
-    totalPartTwo += checkLowest(game)
-  })
+    totalPartTwo += checkLowest(game);
+  });
 
-console.log('Part 1', totalPartOne)
-console.log('Part 1', totalPartTwo)
+  console.log("Part 1", totalPartOne);
+  console.log("Part 1", totalPartTwo);
 });
